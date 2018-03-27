76bb54000051576cf6bc010d0c475e5adf66dcc3
nevingv <nevingeorge3d@gmail.com>
2018-03-27 11:00:47 +0530

    Add files via upload

--- api.py
+++ api.py
@@ -4,13 +4,27 @@ from naiveBayesClassifier.trainer import Trainer
 from naiveBayesClassifier.classifier import Classifier
 from flask import Flask
 from flask import request, jsonify
+
 app = Flask(__name__)
+
+
 
 log_file = None
 
-@app.route("/api/diseaseclassifier", methods=['GET'])
+class CustomType:
+    def __init__(self, title, text):
+        self.disease = title
+        self.probability = text
+
+    def toJSON(self):
+        '''
+        Serialize the object custom object
+        '''
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+@app.route("/api/diseaseclassifier", methods=['POST'])
 def post_logfile():
-   if request.method == 'GET':
+   if request.method == 'POST':
         log_file = request.args['symptom']
         print (log_file)
         diseaseclassifier = Trainer(tokenizer) #STARTS CLASIFIERS
@@ -21,10 +35,13 @@ def post_logfile():
         diseaseclassifier = Classifier(diseaseclassifier.data, tokenizer)
         classification = diseaseclassifier.classify(log_file) #CLASIFY INPUT
         print classification
-        
-        return json.dumps(dict(classification))
+        result = []
+        for item in classification:
+            obj = CustomType(item[0],item[1])
+            result.append(json.loads(obj.toJSON()))
+        # return json.dumps(OrderedDict(classification))
+        return json.dumps(result,indent=4)
     
 
 if __name__ == "__main__":
-    app.run(port=5000)
-    
+    app.run(host='0.0.0.0',port=5000)
